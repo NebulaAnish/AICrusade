fde4040a1d84570a65d195c0f3fd47a1ad78e239
Nebula <nebulaanish@gmail.com>
2023-09-03 18:54:13 +0545

    CRUD operations for transformers

--- backend/main/views.py
+++ backend/main/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework import generics, filters, status
 # Create your views here.
+from .models import Transformer
+from .serializers import TransformerSerializer
+
+class TransformerListView(generics.ListCreateAPIView):
+    queryset = Transformer.objects.all()
+    serializer_class = TransformerSerializer
+    
+class TransformerRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Transformer.objects.all()
+    serializer_class = TransformerSerializer
