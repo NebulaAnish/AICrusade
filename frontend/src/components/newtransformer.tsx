70b1c351171756c58cb785f6f97ede9d019af6b4
Manjul <manjultamrakar4@gmail.com>
2023-09-03 18:22:04 +0545

    fix added map placeholder

--- frontend/src/components/newtransformer.tsx
+++ frontend/src/components/newtransformer.tsx
@@ -12,17 +12,22 @@ const TransformerForm = () => {
       };
 
   return (
-    <div className="flex h-fit flex-col rounded bg-zinc-300 drop-shadow-xl justify-center items-center px-[5rem] relative top-10 left-10">
+    <div className='flex h-fit flex-col rounded bg-zinc-300 drop-shadow-xl justify-center items-center px-[1rem] relative top-10 left-10'>
+    <div className="h-[50vh] w-[65vw]">
+        placeholder
+    </div>
+    <div className="flex h-fit w-[100%] flex-col justify-center items-center ">
         <form onSubmit={handleSubmit}>
             {fields.map((field) => (
-                <div className="px-3 py-1 self-center" key={field}>
+                <div className="px-3 py-1 w-[60vw]" key={field}>
                     <Input placeholder={field} />
                 </div>
             ))}
-            <div className="p-3 self-center">
+            <div className="p-3 ">
                 <Button className="w-fill-available" type='submit' >Confirm</Button>
             </div>
         </form>
+    </div>
     </div>
   );
 };
