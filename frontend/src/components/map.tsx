fea2b47394c6a1b19114640529010d9ce2ba39aa
cceer <sisir.sital@gmail.com>
2023-09-03 16:39:11 +0545

    feat: added map phase 1

--- frontend/src/components/map.tsx
+++ frontend/src/components/map.tsx
@@ -1,23 +1,53 @@
 import ReactMapboxGl, { Layer, Feature } from 'react-mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
+import { Coordinates } from '../../types/types';
+
+import { Marker } from 'react-mapbox-gl';
 
 const Map = ReactMapboxGl({
     accessToken: process.env.MAPBOX_ACCESS_TOKEN || '',
 });
 
+export interface MapProps {
+    width: string;
+    height: string;
+    transformerMarkerCoordinates: Coordinates[];
+    markerUrl: string;
+    center: Coordinates;
+}
 // in render()
-export default () => {
+const MapComponent = ({ width, height, transformerMarkerCoordinates, markerUrl, center }: MapProps) => {
     return (
         <Map
             style="mapbox://styles/mapbox/streets-v9"
             containerStyle={{
-                height: '100vh',
-                width: '100vw',
+                height,
+                width,
             }}
+            // center={[27.734086, 85.347002] }
         >
+            {transformerMarkerCoordinates.map((marker, i) => (
+                <Marker key={i} coordinates={marker} anchor="bottom">
+                    <img height={'40px'} width={'40px'} src={markerUrl} />
+                </Marker>
+            ))}
+
             <Layer type="symbol" id="marker" layout={{ 'icon-image': 'marker-15' }}>
-                <Feature coordinates={[-0.481747846041145, 51.3233379650232]} />
+                <Feature coordinates={[27.714881, 85.312914]} />
             </Layer>
         </Map>
     );
 };
+
+export default MapComponent;
+
+// import { Popup } from "react-mapbox-gl";
+
+// <Popup
+//   coordinates={[-0.13235092163085938,51.518250335096376]}
+// onClick: ()=>{}
+//   offset={{
+//     'bottom-left': [12, -38],  'bottom': [0, -38], 'bottom-right': [-12, -38]
+//   }}>
+//   <h1>Popup</h1>
+// </Popup>
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,18 +1,31 @@
 import { Input } from '@/components/ui/input';
 import Map from '@/components/map';
-import dynamic from "next/dynamic";
+import dynamic from 'next/dynamic';
+import { Coordinates } from '../../types/types';
 
 export default function Home() {
-    const MapWithNoSSR = dynamic(() => import("@/components/map"), {
-        ssr: false
-      });
-    
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Input />
-      <div id="map">
-        <MapWithNoSSR />
-      </div>
-    </main>
-  )
+    const MapWithNoSSR = dynamic(() => import('@/components/map'), {
+        ssr: false,
+    });
+
+    const transormerCoordinates: Coordinates[] = [
+        [27.714889, 85.312917],
+        [27.730312, 85.355778],
+        [27.734086, 85.347002],
+    ];
+
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <Input />
+            <div id="map">
+                <MapWithNoSSR
+                    center={[27.714889, 85.312917]}
+                    markerUrl="https://cdn-icons-png.flaticon.com/512/649/649813.png"
+                    transformerMarkerCoordinates={transormerCoordinates}
+                    height={'60vh'}
+                    width={'100vw'}
+                />
+            </div>
+        </main>
+    );
 }
