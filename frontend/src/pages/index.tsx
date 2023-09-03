b0b1ad36a4dbdd4b2ed201c47d2673ce6c493bed
Manjul <manjultamrakar4@gmail.com>
2023-09-03 14:29:37 +0545

    added map component

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,9 +1,18 @@
 import { Input } from '@/components/ui/input';
+import Map from '@/components/map';
+import dynamic from "next/dynamic";
 
 export default function Home() {
+    const MapWithNoSSR = dynamic(() => import("@/components/map"), {
+        ssr: false
+      });
+    
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Input />
+      <div id="map">
+        <MapWithNoSSR />
+      </div>
     </main>
   )
 }
