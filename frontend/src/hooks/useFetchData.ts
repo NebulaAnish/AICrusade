867905bafb7b24e0d3a9333a114e7bf16c202fba
cceer <sisir.sital@gmail.com>
2023-09-04 00:14:03 +0545

    null

---
+++ frontend/src/hooks/useFetchData.ts
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import { Transformer } from "../../types/types";
+import useAxios from "./useAxios";
+
+const useFetchData = () => {
+  const [data, setData] = useState<Array<Transformer>>();
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string>("");
+  const axios = useAxios();
+
+  const fetchAllData = async () => {
+    try {
+      setIsLoading(true);
+      const { data } = await axios.get(`transformers`);
+      setData(data);
+    } catch (err: any) {
+      setError(err);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return { error, data, isLoading, fetchAllData };
+};
+
+export default useFetchData;
