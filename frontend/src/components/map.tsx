47bd34087c5f4700ffc44d0577ecbc83d7f2c294
cceer <sisir.sital@gmail.com>
2023-09-03 15:04:53 +0545

    feat: map added

--- frontend/src/components/map.tsx
+++ frontend/src/components/map.tsx
@@ -1,27 +1,23 @@
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
-import 'leaflet/dist/leaflet.css'
-import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css'
-import "leaflet-defaulticon-compatibility";
+import ReactMapboxGl, { Layer, Feature } from 'react-mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
 
-const API_TOKEN = process.env.MAPBOX_ACCESS_TOKEN;
+const Map = ReactMapboxGl({
+    accessToken: process.env.MAPBOX_ACCESS_TOKEN || '',
+});
 
-const Map = () => {
-  return (
-    <MapContainer
-      center={[40.8054, -74.0241]}
-      zoom={14}
-      scrollWheelZoom={false}
-      style={{ height: "100%", width: "100%" }}
-    >
-      <TileLayer
-        url={`https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.MAPBOX_ACCESS_TOKEN}`}
-        attribution='Map data &copy; <a href=&quot;https://www.openstreetmap.org/&quot;>OpenStreetMap</a> contributors, <a href=&quot;https://creativecommons.org/licenses/by-sa/2.0/&quot;>CC-BY-SA</a>, Imagery &copy; <a href=&quot;https://www.mapbox.com/&quot;>Mapbox</a>'
-      />
-      <Marker position={[40.8054, -74.0241]} draggable={true}>
-        <Popup>Hey ! I live here</Popup>
-      </Marker>
-    </MapContainer>
-  )
-}
-
-export default Map;
+// in render()
+export default () => {
+    return (
+        <Map
+            style="mapbox://styles/mapbox/streets-v9"
+            containerStyle={{
+                height: '100vh',
+                width: '100vw',
+            }}
+        >
+            <Layer type="symbol" id="marker" layout={{ 'icon-image': 'marker-15' }}>
+                <Feature coordinates={[-0.481747846041145, 51.3233379650232]} />
+            </Layer>
+        </Map>
+    );
+};
