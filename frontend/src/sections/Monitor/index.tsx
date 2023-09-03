7d8b290a1ad97dc87cd744383dfaba04fcdb5f0a
Manjul <manjultamrakar4@gmail.com>
2023-09-03 22:07:15 +0545

    feat: search finally done

--- frontend/src/sections/Monitor/index.tsx
+++ frontend/src/sections/Monitor/index.tsx
@@ -1,12 +1,21 @@
+import { useState } from 'react';
+import SearchBar from '@/components/searchbar';
 import dynamic from 'next/dynamic';
 import transformers from '../../../db/transformers';
 
 export default function Home() {
+    const [center, setCenter] = useState<[number, number]>([85, 27.1]);
     const MapWithNoSSR = dynamic(() => import('@/components/map'));
+
+    const handleCenterChange = (center: [number, number]) => {
+        const centerRev:[number, number] = center.reverse() as [number, number];
+        setCenter(centerRev);
+    };
 
     return (
         <div id="map" className="overflow-hidden">
-            <MapWithNoSSR center={[85, 27.1]} transformers={transformers} height={'70vh'} width={'100vw'} />
+            <SearchBar onLocationSelect={handleCenterChange} />
+            <MapWithNoSSR center={center} transformers={transformers} height={'70vh'} width={'100vw'} />
         </div>
       );
 }
